881379bc0d8bf06a915858aba4c4e5a9d66ee42c
Astern2112 <sternaarond@gmail.com>
2022-09-25 12:56:20 -0400

    able to deply and verify on testnet

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,5 +1,6 @@
 const { network } = require("hardhat")
 const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log, get } = deployments
@@ -14,17 +15,19 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
     }
 
+    const args = [ethUsdPriceFeedAddress]
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [ethUsdPriceFeedAddress], //put price feed address
+        args: args, //put price feed address
         log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
     })
 
     if (
         !developmentChains.includes(network.name) &&
         process.env.ETHERSCAN_API_KEY
     ) {
-        // verify
+        await verify(fundMe.address, args)
     }
     log("----------------------------------------------------------------")
 }
