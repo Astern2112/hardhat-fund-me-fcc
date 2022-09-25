f15df245cfe3f2defc039387a01b5ab876a16a61
Astern2112 <sternaarond@gmail.com>
2022-09-25 12:42:33 -0400

    init commit, able to deploy mocks to Local

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,31 @@
+const { network } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log, get } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress], //put price feed address
+        log: true,
+    })
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        // verify
+    }
+    log("----------------------------------------------------------------")
+}
+module.exports.tags = ["all", "fundme"]
